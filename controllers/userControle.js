b26a8485e5289951a7a261e5cdf5575ea435014d
WORLDWAR03 <manjumurali3941@gmail.com>
2023-12-12 23:22:48 +0400

    cancelBookings

--- controllers/userControle.js
+++ controllers/userControle.js
@@ -1,7 +1,12 @@
 const COURT = require('../models/courtSchenma')
 const COURT_SCHEDULES = require('../models/courtTimingSchema')
+const USER = require('../models/userSchema')
 const mongoose =require('mongoose')
 const ObjectId =require('mongoose').Types.ObjectId
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+
 
 
 
@@ -51,7 +56,6 @@ const getAvailableSlots=(req,res)=>{
 
     ])
        .then((resp)=>{
-            console.log(resp,'tttuooooorrrrrrrrrcccccccj');
             res.status(200).json({resp})
         }) .catch((err)=>{
             console.log(err);
@@ -151,6 +155,99 @@ const getPreviousBookings=(req,res)=>{
 
 }
 
+const forgetpassword=(req,res)=>{
+    try {
+        const {email} = req.body
+        USER.findOne({email:email}).then((user)=>{
+            console.log(user);
+            if(!user){
+                return res.send({status:'user not existed'})
+            }
+            if(user){
+            const token = jwt.sign({id:user._id}, process.env.JWT ,{expiresIn: "1d"})
+            const transporter = nodemailer.createTransport({
+                service:"Gmail",
+                auth: {
+                  user: "slook.in.here@gmail.com",
+                  pass: "guwp zcuj nmrr cbfp",
+                },
+              });
+    
+                const info =  transporter.sendMail({
+                  from: "slook.in.here@gmail.com", 
+                  to: `${user.email}`, 
+                  subject: "Reset your password✔",
+                  text: `http://localhost:3000/reset-password/${user._id}/${token}`, 
+                  html: `<b>use this link to reset your password:http://localhost:3000/reset-password/${user._id}/${token}</b>`, 
+                });
+                console.log("Message sent: %s", info.messageId);
+    
+                return res.send({staus: 'success'})
+            }
+          })
+             
+    } catch (error) {
+        
+    }
+   
+  
+    
+}
 
+const resetPassword=(req,res)=>{
+    try {
+         req.query.id
+        const {token,login,id}=req.body;
+        const {password} =login
+        console.log(token,id,password,'rrrrrrrrrrrrreeeeeeeeeeeeeeeeeeeeeeeeeetgggggggg');
 
-module.exports =  {getBookingPage, getAvailableSlots, getMyBookings, getPreviousBookings}
+        jwt.verify(token,process.env.JWT,(err ,decoded)=>{
+            if(err){
+                return res.json({status:"Error with token"})
+            }else{
+                bcrypt.hash(password,10)
+                .then(hash=>{
+                    USER.findByIdAndUpdate({_id:id},{password:hash})
+                    .then((resp)=>res.send({status:"success"}))
+                    .catch((resp)=>res.send({status:err}))
+                })
+            }
+        })
+    } catch (error) {
+    
+    }
+   
+}
+
+const getThisScheduleData=(req,res)=>{
+    try {
+        const{id}=req.query
+    COURT_SCHEDULES.aggregate([
+        {$match:{
+            _id:new ObjectId(id),
+            bookedBy:new ObjectId(req.userId)
+        }},
+        {$lookup:{
+            from:"courts",
+            localField:"courtId",
+            foreignField:"_id",
+            as:'court'
+        }},
+        {$project:{
+            _id:1,
+            date:1,
+            slot:1,
+            cost:1,
+            cancellation:1,
+            court:{$arrayElemAt:["$court",0]}
+
+        }}
+    ]).then((resp)=>{
+        res.status(200).json(resp)
+    })
+    } catch (error) {
+        
+    }
+}
+
+module.exports =  {getBookingPage, getAvailableSlots, getMyBookings, getPreviousBookings, forgetpassword, resetPassword, getThisScheduleData}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const {getBookingPage, getAvailableSlots, getMyBookings, getPreviousBookings} =require('../controllers/userControle')
+const {getBookingPage, getAvailableSlots, getMyBookings, getPreviousBookings, forgetpassword, resetPassword, getThisScheduleData} =require('../controllers/userControle')
 var router = express.Router();
 const userAuth = require('../middleWare/userAuth');
 
@@ -8,8 +8,9 @@ router.get('/getBookingPage', userAuth, getBookingPage);
 router.get('/getAvailableSlots', userAuth, getAvailableSlots);
 router.get('/getMyBookings',userAuth, getMyBookings);
 router.get('/getPreviousBookings',userAuth, getPreviousBookings); 
-
-
+router.post('/forget-password', forgetpassword);
+router.post('/reset-password',resetPassword);
+router.get('/getThisScheduleData',userAuth, getThisScheduleData)
 
 
 
